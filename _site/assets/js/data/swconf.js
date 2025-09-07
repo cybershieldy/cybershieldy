64b0b2c7ac2bef0dc76a6c521d924bf5b9002765
Emin Savaylov <cyberdersler@gmail.com>
2025-09-07 15:23:16 +0400

    update site

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1757244118',resources: [
+    cacheName: 'chirpy-1757244196',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
--- _tabs/Test.md
+++ _tabs/Test.md
@@ -1,5 +1,5 @@
 ---
-title: Bilik Testi
+title: Test
 ---
 
 # Kibertəhlükəsizlik Bilik Testi
