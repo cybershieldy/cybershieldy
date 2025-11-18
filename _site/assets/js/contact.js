b730b5d8c9cc85f455b4f3f3f9df36b40404e7da
Emin Savaylov <cyberdersler@gmail.com>
2025-11-18 20:07:32 +0400

    update site

--- _site/assets/js/contact.js
+++ _site/assets/js/contact.js
@@ -1,7 +1,13 @@
 function sendWhatsApp() {
-  let name = document.getElementById('name').value;
-  let phone = document.getElementById('phone').value;
-  let service = document.getElementById('service').value;
+  let name = document.getElementById('name').value.trim();
+  let phone = document.getElementById('phone').value.trim();
+  let service = document.getElementById('service').value.trim();
+
+  // Xanaların yoxlanması
+  if (!name || !phone || !service) {
+    alert("Zəhmət olmasa bütün xanaları doldurun!");
+    return; // WhatsApp'a getməsin
+  }
 
   let msg = `Salam, mən müraciət etmək istəyirəm:%0A` +
             `Ad Soyad: ${name}%0A` +
--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763481583',resources: [
+    cacheName: 'chirpy-1763482050',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
