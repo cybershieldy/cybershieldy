cb5543509088fb02915ff1bf60d6b634cb21c3b0
Emin Savaylov <cyberdersler@gmail.com>
2026-03-04 14:58:28 +0400

    update site

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1772621614',resources: [
+    cacheName: 'chirpy-1772621905',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
